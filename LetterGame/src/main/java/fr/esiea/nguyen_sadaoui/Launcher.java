5912986ea20089cf7d83099efeab9024ae418df7
Swan <sadaoui@et.esiea.fr>
2017-01-24 15:23:22 +0100

    test2

--- LetterGame/src/main/java/fr/esiea/nguyen_sadaoui/Launcher.java
+++ LetterGame/src/main/java/fr/esiea/nguyen_sadaoui/Launcher.java
@@ -3,6 +3,6 @@ package fr.esiea.nguyen_sadaoui;
 public class Launcher {
 
 	public static void main(String args[]){
-		System.out.println("Launching Lettter Game");
+		System.out.println("Launching Letter Game");
 	}
 }
